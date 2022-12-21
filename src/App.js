cefe0a9ea17fc5d395f13dfc70b1d2e9657a4387
HARRY OMOLLOH <harryomollo95@gmail.com>
2022-12-21 15:59:31 +0300

    deleted unnecesary shit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>Hello world</div>
   );
 }
 
 export default App;
+
+/**
+ * Portfolio Map
+ * 
+ * On the header section
+ * - Resume link
+ * - Contact info (Linkedin, Twitter, Github)
+ * - Name
+ * 
+ * On the About me section
+ * - Get words from ChatGPT
+ * - Short and sweet About
+ * 
+ * On the skills section
+ * - Icons for languages and technologies
+ * 
+ * On the projects section
+ *  - HtoStudios
+ *  - Studyhelp
+ *  - Blockchain projects
+ *  - Display two links for source and for live preview
+ *  - Projects should have a HQ readme on Github
+ * 
+ * On the footer section
+ *  - Contact links and resume again
+ * 
+ * On the blog section
+ *  - When I create the HtoStudios API  I will serve blogs on my portfolio site
+ * 
+ * Creative tips
+ * - Add icons to skills i.e. Orange HTML icon as HTML skill
+ * - Domain name with projects as subdomains
+ * - Dark and light theme
+ * 
+ * Future of Portfolio
+ * - Create a contact section for freelancing and business enquiries
+ * 
+ * Tips and Trics 
+ * - Remove dates on projects to avoid judgement
+ * - Projects come before skills
+ *
+ * My Skills
+ * Software Developer
+ * Full-stack Web Developer
+ * Blockchain Developer
+ * 
+ * Title
+ * Software developer and author at HtoStudios
+ * 
+ * Research?
+ * - How to present your freelance project on your Portfolio
+ */
