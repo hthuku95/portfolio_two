93b5d0a5cfab082e44cf7fb0557274468c4f5b17
HARRY OMOLLOH <hthuku95@gmail.com>
2023-01-27 01:32:47 +0300

    Created About component

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import {Box,Typography} from "@mui/material";
+
+const About = () =>{
+    return (
+        <Box>
+            <Typography variant="body1">
+                Hello from the other side, I must have called a thousand times
+            </Typography>
+        </Box>
+    )
+}
+
+export default About;
