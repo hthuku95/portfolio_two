f68e1851947b13c963559b491a28bdd6357d8c80
HARRY OMOLLOH <hthuku95@gmail.com>
2023-01-30 00:51:27 +0300

    Created component

---
+++ src/components/Skills.js
@@ -0,0 +1,13 @@
+import {Container,Typography} from "@mui/material"
+
+const Skills = () => {
+    return (
+        <Container>
+            <Typography variant="h3">
+                My Skills
+            </Typography>
+        </Container>
+    )
+}
+
+export default Skills;
