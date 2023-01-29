dd3f7d8c3ebe75dc6e19574e0c0e20c6878d6b30
HARRY OMOLLOH <hthuku95@gmail.com>
2023-01-30 00:54:02 +0300

    Added component data

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,31 @@
-import {Box,Typography} from "@mui/material";
+import {Container,Grid,Typography} from "@mui/material";
 
 const About = () =>{
     return (
-        <Box>
-            <Typography variant="body1">
-                Hello from the other side, I must have called a thousand times
-            </Typography>
-        </Box>
+        <Container>
+            <Grid container spacing={2} columns={16}>
+                <Grid item xs={16} sm={16} md={8} lg={8} xl={8} height="100vh">
+                <Typography variant="h3">
+                        About Me
+                    </Typography>
+                </Grid>
+                <Grid item xs={16} sm={16} md={8} lg={8} xl={8} height="100vh">
+                    <Typography variant="body1" sx={{ p: 10 }}>
+                        I'll wait forever, thats what we said, 16th of september lying in my bed
+                        I'll wait forever, thats what we said, 16th of september lying in my bed
+                        I'll wait forever, thats what we said, 16th of september lying in my bed
+                        I'll wait forever, thats what we said, 16th of september lying in my bed
+                        I'll wait forever, thats what we said, 16th of september lying in my bed
+                        I'll wait forever, thats what we said, 16th of september lying in my bed
+                        I'll wait forever, thats what we said, 16th of september lying in my bed
+                        I'll wait forever, thats what we said, 16th of september lying in my bed
+                        I'll wait forever, thats what we said, 16th of september lying in my bed
+                        I'll wait forever, thats what we said, 16th of september lying in my bed
+                        I'll wait forever, thats what we said, 16th of september lying in my bed
+                    </Typography>
+                </Grid>
+            </Grid>
+        </Container>
     )
 }
 
