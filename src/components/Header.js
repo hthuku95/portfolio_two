b7c9b063b2de6943f6a2d83bcccff81faf9435c1
HARRY OMOLLOH <hthuku95@gmail.com>
2023-01-30 00:53:23 +0300

    Updated Header Padding

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,12 @@ const Header = () => {
         <Container>
             <Grid container spacing={2} columns={16}>
                 <Grid item xs={16} sm={16} md={8} lg={8} xl={8} height="100vh">
-                    <Typography variant="h2" sx={{ p: 8 }}>
+                    <Typography variant="h2" sx={{ p: 13 }}>
                         Full Stack Web & Blockchain Developer
                     </Typography>
                 </Grid>
                 <Grid item xs={16} sm={16} md={8} lg={8} xl={8} height="100vh">
-  
+                  
                 </Grid>
             </Grid>
         </Container>
