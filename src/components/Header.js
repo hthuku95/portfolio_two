33d7fc14277a2575877081fbb5029dd433380a0b
HARRY OMOLLOH <hthuku95@gmail.com>
2023-01-30 03:39:18 +0300

    Added section Id

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,10 @@ import {Container,Grid,Typography} from "@mui/material"
 
 const Header = () => {
     return (
-        <Container>
+        <Container id='section1'>
             <Grid container spacing={2} columns={16}>
                 <Grid item xs={16} sm={16} md={8} lg={8} xl={8} height="100vh">
-                    <Typography variant="h2" sx={{ p: 13 }}>
+                    <Typography variant="h2" sx={{ p: 12 }}>
                         Full Stack Web & Blockchain Developer
                     </Typography>
                 </Grid>
