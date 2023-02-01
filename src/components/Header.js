9c72b05182a9c6f1d4bceb43ce9b01932f528427
HARRY OMOLLOH <hthuku95@gmail.com>
2023-02-01 13:12:26 +0300

    Added section for social media icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
-import {Container,Grid,Typography} from "@mui/material"
+import {Container,Grid,Typography,Box} from "@mui/material";
 
 const Header = () => {
     return (
         <Container id='section1'>
             <Grid container spacing={2} columns={16}>
                 <Grid item xs={16} sm={16} md={8} lg={8} xl={8} height="100vh">
-                    <Typography variant="h2" sx={{ p: 12 }}>
+                    <Typography variant="h2" sx={{ p: 4 }}>
                         Full Stack Web & Blockchain Developer
                     </Typography>
+                    <Box>
+                        Social media icons go here
+                    </Box>
                 </Grid>
                 <Grid item xs={16} sm={16} md={8} lg={8} xl={8} height="100vh">
                   
