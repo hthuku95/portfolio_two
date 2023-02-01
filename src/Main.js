fe81f50a9bfdeaf46c0827339307033d3a592761
HARRY OMOLLOH <hthuku95@gmail.com>
2023-02-01 13:10:05 +0300

    Added projects about and footer components

--- src/Main.js
+++ src/Main.js
@@ -2,16 +2,20 @@ import Nav from './components/Nav';
 import Header from './components/Header';
 import About from './components/About';
 import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
 import {Box} from '@mui/material';
 
 const Main = ()=>{
 
     return (
-        <Box>
+        <Box sx={{backgroundColor:"custom.grey"}}>
             <Nav />
             <Header />
-            <About />
             <Skills />
+            <Projects/>
+            <About />
+            <Footer/>
         </Box>
     )
 }
