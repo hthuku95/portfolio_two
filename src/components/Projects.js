8a2a352a5cf59a4611293e6bd394f1e920c148bc
HARRY OMOLLOH <hthuku95@gmail.com>
2023-02-01 13:13:31 +0300

    Created projects component

---
+++ src/components/Projects.js
@@ -0,0 +1,13 @@
+import {Container,Typography} from '@mui/material';
+
+const Projects = () =>{
+ return (
+    <Container id='section3' height="100vh">
+        <Typography variant='h3'>
+            Projects
+        </Typography>
+    </Container>
+ )
+}
+
+export default Projects;
