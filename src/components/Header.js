d74d3601d31f6a9ab61c71137cca847ac5fc1df1
HARRY OMOLLOH <hthuku95@gmail.com>
2023-02-06 04:32:04 +0300

    Completed header section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,67 @@
-import {Container,Grid,Typography,Box} from "@mui/material";
+import {Container,Grid,Typography,Link} from "@mui/material";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import {ReactComponent as HeaderLogo} from '../svg/code.svg';
+
+const styles = {
+    svg: {
+      width: '100%',
+      height: '100%',
+    },
+  };
 
 const Header = () => {
     return (
         <Container id='section1'>
             <Grid container spacing={2} columns={16}>
                 <Grid item xs={16} sm={16} md={8} lg={8} xl={8} height="100vh">
-                    <Typography variant="h2" sx={{ p: 4 }}>
+                    <Typography variant="h2" sx={{
+                         p:4,
+                         color:'custom.blue'
+                         }}>
                         Full Stack Web & Blockchain Developer
                     </Typography>
-                    <Box>
-                        Social media icons go here
-                    </Box>
+                    <Typography variant="body1" sx={{
+                        marginLeft:4,
+                        marginBottom:4,
+                        color: 'custom.violet'
+                    }}
+                    gutterBottom='true'
+                    >
+                        Full Stack Web & Blockchain Developer with expertise in web development and blockchain technology. Skilled in creating secure and innovative solutions. Committed to delivering user-centered outcomes and always seeking new challenges.
+                    </Typography>
+                    <Typography sx={{
+                        marginLeft:4,
+                        color:'custom.blue',
+                        }}
+                        gutterBottom='true'
+                        >
+                            <Link href="#" underline="none" color="inherit" sx={{
+                                marginRight:2
+                            }}>
+                                <GitHubIcon fontSize="large"/>
+                            </Link>
+                            <Link href="#" underline="none" color="inherit" sx={{
+                                marginRight:2
+                            }}>
+                                <LinkedInIcon fontSize="large"/>
+                            </Link>
+                            <Link href="#" underline="none" color="inherit" sx={{
+                                marginRight:2
+                            }}>
+                                <TwitterIcon fontSize="large"/>
+                            </Link>
+                            <Link href="#" underline="none" color="inherit" sx={{
+                                marginRight:2
+                            }}>
+                                <FileDownloadIcon fontSize="large"/>
+                            </Link>
+                    </Typography>
                 </Grid>
                 <Grid item xs={16} sm={16} md={8} lg={8} xl={8} height="100vh">
-                  
+                    <HeaderLogo style={styles.svg}/>
                 </Grid>
             </Grid>
         </Container>
