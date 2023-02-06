6e509cbe688ee7dbbaf1a116a9ec1c7d0122a970
HARRY OMOLLOH <hthuku95@gmail.com>
2023-02-06 04:32:44 +0300

    Added project prototypes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,111 @@
+import React,{useState} from 'react';
 import {Container,Typography} from '@mui/material';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
 
 const Projects = () =>{
- return (
-    <Container id='section3' height="100vh">
-        <Typography variant='h3'>
-            Projects
-        </Typography>
-    </Container>
- )
+    const [value, setValue] = useState('1');
+
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
+
+    return (
+        <Container id='section3' height="100vh">
+            <Typography variant='h3' sx={{color:'custom.blue'}} gutterBottom='true'>
+                Projects
+            </Typography>
+            <Box sx={{ width: '100%', typography: 'body1' }}>
+                <TabContext value={value}>
+                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                        <TabList onChange={handleChange} aria-label="lab API tabs example">
+                            <Tab label="All Projects" value="1" sx={{color:'custom.violet'}}/>
+                            <Tab label="Web Projects" value="2" sx={{color:'custom.violet'}}/>
+                            <Tab label="Blockchain" value="3" sx={{color:'custom.violet'}}/>
+                        </TabList>
+                    </Box>
+
+                    <TabPanel value="1" sx={{color:'custom.violet'}}>
+                        <Card sx={{ maxWidth: 345 }}>
+                            <CardMedia
+                                sx={{ height: 140 }}
+                                image="/static/images/cards/contemplative-reptile.jpg"
+                                title="green iguana"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                Lizard
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                Lizards are a widespread group of squamate reptiles, with over 6,000
+                                species, ranging across all continents except Antarctica
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Share</Button>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
+                    </TabPanel>
+
+                    <TabPanel value="2" sx={{color:'custom.violet'}}>
+                        <Card sx={{ maxWidth: 345 }}>
+                                <CardMedia
+                                    sx={{ height: 140 }}
+                                    image="/static/images/cards/contemplative-reptile.jpg"
+                                    title="green iguana"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                    Lizard
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small">Share</Button>
+                                    <Button size="small">Learn More</Button>
+                                </CardActions>
+                            </Card>
+                    </TabPanel>
+
+                    <TabPanel value="3" sx={{color:'custom.violet'}}>
+                        <Card sx={{ maxWidth: 345 }}>
+                                <CardMedia
+                                    sx={{ height: 140 }}
+                                    image="/static/images/cards/contemplative-reptile.jpg"
+                                    title="green iguana"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                    Lizard
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small">Share</Button>
+                                    <Button size="small">Learn More</Button>
+                                </CardActions>
+                            </Card>
+                    </TabPanel>
+
+                </TabContext>
+            </Box>
+        </Container>
+    )
 }
 
 export default Projects;
