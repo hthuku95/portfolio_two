b20162b477bc5d68f6a4d82e3adaac8fde153ccf
HARRY OMOLLOH <hthuku95@gmail.com>
2023-02-06 04:33:19 +0300

    Added skills prototype

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,13 +1,84 @@
-import {Container,Typography} from "@mui/material"
+import {Container,Typography,Grid} from "@mui/material"
 
 const Skills = () => {
     return (
-        <Container id='section2'>
-            <Typography variant="h3">
-                My Skills
+        <Container id='section2' gutterBottom='true'>
+            <Typography variant="h3" sx={{color:'custom.blue'}} gutterBottom='true'>
+                Skills
             </Typography>
+            <Typography variant="h5" sx={{color:'custom.violet'}} gutterBottom='true'>
+                Technologies I work with 
+            </Typography>
+            <Grid container spacing={2} columns={16}>
+                <Grid item xs={8} sm={8} md={4} lg={4} xl={4} >
+                    <Typography variant="body1">skill</Typography>
+                </Grid>
+                <Grid item xs={8} sm={8} md={4} lg={4} xl={4} >
+                    <Typography variant="body1">skill</Typography>
+                </Grid>
+                <Grid item xs={8} sm={8} md={4} lg={4} xl={4} >
+                    <Typography variant="body1">skill</Typography>
+                </Grid>
+                <Grid item xs={8} sm={8} md={4} lg={4} xl={4} >
+                    <Typography variant="body1">skill</Typography>
+                </Grid>
+                <Grid item xs={8} sm={8} md={4} lg={4} xl={4} >
+                    <Typography variant="body1">skill</Typography>
+                </Grid>
+                <Grid item xs={8} sm={8} md={4} lg={4} xl={4} >
+                    <Typography variant="body1">skill</Typography>
+                </Grid>
+                <Grid item xs={8} sm={8} md={4} lg={4} xl={4} >
+                    <Typography variant="body1">skill</Typography>
+                </Grid>
+                <Grid item xs={8} sm={8} md={4} lg={4} xl={4} >
+                    <Typography variant="body1">skill</Typography>
+                </Grid>
+                <Grid item xs={8} sm={8} md={4} lg={4} xl={4} >
+                    <Typography variant="body1">skill</Typography>
+                </Grid>
+                <Grid item xs={8} sm={8} md={4} lg={4} xl={4} >
+                    <Typography variant="body1">skill</Typography>
+                </Grid>
+                <Grid item xs={8} sm={8} md={4} lg={4} xl={4} >
+                    <Typography variant="body1">skill</Typography>
+                </Grid>
+                <Grid item xs={8} sm={8} md={4} lg={4} xl={4} >
+                    <Typography variant="body1">skill</Typography>
+                </Grid>
+                <Grid item xs={8} sm={8} md={4} lg={4} xl={4} >
+                    <Typography variant="body1">skill</Typography>
+                </Grid>
+                <Grid item xs={8} sm={8} md={4} lg={4} xl={4} >
+                    <Typography variant="body1">skill</Typography>
+                </Grid>
+                <Grid item xs={8} sm={8} md={4} lg={4} xl={4} >
+                    <Typography variant="body1">skill</Typography>
+                </Grid>
+                <Grid item xs={8} sm={8} md={4} lg={4} xl={4} >
+                    <Typography variant="body1">skill</Typography>
+                </Grid>
+            </Grid>
         </Container>
     )
 }
 
 export default Skills;
+
+/**
+Solidity
+Python
+Javascript
+TypeScript
+React.js
+Next.js
+Django
+Node.js
+Express.js
+Postgresql
+MongoDB
+HTML
+CSS 
+Bootstrap
+mui
+GIT*/
