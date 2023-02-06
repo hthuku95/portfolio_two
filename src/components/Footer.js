1baf0051dd16e7c5de93fff0a0522d0025455d69
HARRY OMOLLOH <hthuku95@gmail.com>
2023-02-06 04:31:28 +0300

    completed footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,8 @@
 import {Container,Typography,Link,Box} from '@mui/material'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
 
 function Copyright() {
     return (
@@ -17,16 +21,40 @@ const Footer = () => {
     return (
         <Box component="footer" id='section5' sx={{ backgroundColor: 'custom.dark', color:'custom.white', py: 6 }}>
         <Container maxWidth="lg">
-          <Typography variant="h6" align="center" gutterBottom>
+          <Typography variant="h6" align="center" gutterBottom='true' sx={{
+            marginBottom:4
+          }}>
             Get in touch
           </Typography>
           <Typography
-            variant="subtitle1"
+            variant="body1"
             align="center"
             color="custom.white"
             component="p"
+            sx={{
+              marginBottom:4
+            }}
           >
-            Great things happen when creativity meets Imagination
+            <Link href="#" underline="none" color="inherit" sx={{
+              marginRight:2
+            }}>
+                <GitHubIcon fontSize="large"/>
+            </Link>
+            <Link href="#" underline="none" color="inherit" sx={{
+              marginRight:2
+            }}>
+                <LinkedInIcon fontSize="large"/>
+            </Link>
+            <Link href="#" underline="none" color="inherit" sx={{
+              marginRight:2
+            }}>
+                <TwitterIcon fontSize="large"/>
+            </Link>
+            <Link href="#" underline="none" color="inherit" sx={{
+              marginRight:2
+            }}>
+                <FileDownloadIcon fontSize="large"/>
+            </Link>
           </Typography>
           <Copyright />
         </Container>
