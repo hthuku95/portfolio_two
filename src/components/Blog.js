26ca06ecbc66f9527c0a8ec7339d95ca95cea2d7
HARRY OMOLLOH <hthuku95@gmail.com>
2023-03-02 10:18:43 +0300

    Created Blog component

---
+++ src/components/Blog.js
@@ -0,0 +1,15 @@
+import {Container,Typography} from '@mui/material';
+
+const Blog = ()=> {
+
+
+    return (
+        <Container id='section5' height="100vh">
+            <Typography variant='h3' sx={{color:'custom.blue'}} gutterBottom='true'>
+                Blog
+            </Typography>
+        </Container>
+    )
+}
+
+export default Blog;
