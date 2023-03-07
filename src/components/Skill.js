3b4f813b63437c1afa9ba1f7af8e0a27f7fc32eb
HARRY OMOLLOH <hthuku95@gmail.com>
2023-03-07 17:26:01 +0300

    Created the Skill Component

---
+++ src/components/Skill.js
@@ -0,0 +1,17 @@
+import {Box} from '@mui/material';
+
+const Skill = (props) => {
+    return (
+        <Box sx={{
+            p:1,
+            m:2,
+            borderRadius: '16px',
+            width:'100px',
+            height:'100px'
+        }} key={props.key}>
+            <img src={props.source} alt={props.alt} title={props.title} style={{ width: "100%", height: "100%" }}/>
+        </Box>
+    )
+}
+
+export default Skill;
