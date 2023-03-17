ec4f60da6eaba4b6012fc186c76b2d7dce3be8ca
HARRY OMOLLOH <hthuku95@gmail.com>
2023-03-17 16:35:21 +0300

    Added email component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import EmailIcon from '@mui/icons-material/Email';
 
 function Copyright() {
     return (
@@ -35,20 +36,25 @@ const Footer = () => {
               marginBottom:4
             }}
           >
-            <Link href="#" underline="none" color="inherit" sx={{
+            <Link href="https://www.github.com/hthuku95/" underline="none" color="inherit" sx={{
               marginRight:2
             }}>
                 <GitHubIcon fontSize="large"/>
             </Link>
-            <Link href="#" underline="none" color="inherit" sx={{
+            <Link href="https://www.linkedin.com/in/hthuku/" underline="none" color="inherit" sx={{
               marginRight:2
             }}>
                 <LinkedInIcon fontSize="large"/>
             </Link>
-            <Link href="#" underline="none" color="inherit" sx={{
+            <Link href="https://www.twitter.com/blackhatthuku/" underline="none" color="inherit" sx={{
               marginRight:2
             }}>
                 <TwitterIcon fontSize="large"/>
+            </Link>
+            <Link href="mailto: harrythukuomolloh@gmail.com" underline="none" color="inherit" sx={{
+              marginRight:2
+            }}>
+                <EmailIcon fontSize="large"/>
             </Link>
             <Link href="#" underline="none" color="inherit" sx={{
               marginRight:2
