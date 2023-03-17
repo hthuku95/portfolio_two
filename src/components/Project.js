4cd56c58b39115005f9978abb06747b8fe43d1bf
HARRY OMOLLOH <hthuku95@gmail.com>
2023-03-17 16:37:00 +0300

    Added Github icon and visibility icon

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,4 +1,6 @@
-import {Card,CardMedia,CardContent,Typography,CardActions,Button} from '@mui/material';
+import {Card,CardMedia,CardContent,Typography,CardActions,Link,Button} from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import VisibilityIcon from '@mui/icons-material/Visibility';
 
 const Project = (props) => {
 
@@ -22,8 +24,39 @@ const Project = (props) => {
                 </Typography>
             </CardContent>
             <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
+                <Link href={props.githubLink} underline="none"  color="custom.white" sx={{
+
+                }}>
+                    <Button variant="contained" sx={{
+                        padding:'16px',
+                        marginBottom:'16px',
+                        backgroundColor:'custom.dark',
+                        '&:hover': {
+                            backgroundColor: 'custom.dark',
+                          },
+                    }}>
+                        <GitHubIcon fontSize='small'/>
+                        <Typography variant="body2" sx={{
+                            paddingLeft:'8px'
+                        }}>Source Code</Typography>
+                    </Button>
+                </Link>
+                <Link href={props.liveLink} underline="none"color="custom.white">
+                    <Button variant="outlined" sx={{
+                        color:'custom.white',
+                        borderColor:'custom.white',
+                        '&:hover': {
+                            borderColor: 'custom.white',
+                          },
+                        padding:'16px',
+                        marginBottom:'16px'
+                    }}>
+                        <VisibilityIcon fontSize='small'/>
+                        <Typography variant="body2" sx={{
+                            paddingLeft:'8px'
+                        }}>Live Preview</Typography>
+                    </Button>
+                </Link>
             </CardActions>
         </Card>
     )
