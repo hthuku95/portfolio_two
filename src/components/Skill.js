8435efb9c25b46008635195b86a5a390056a78e5
HARRY OMOLLOH <hthuku95@gmail.com>
2023-04-17 00:52:17 +0300

    Updated the Skill component

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,15 +1,39 @@
-import {Box} from '@mui/material';
+import {Box,Grid,Typography} from '@mui/material';
 
 const Skill = (props) => {
     return (
         <Box sx={{
-            p:2,
-            m:2,
+            p:1,
+            m:1,
             borderRadius: '16px',
-            width:'100px',
-            height:'100px'
+            width:'190px',
+            height:'90px',
+            backgroundColor:'custom.blue',
+            color:'custom.white'
         }} key={props.key}>
-            <img src={props.source} alt={props.alt} title={props.title} style={{ width: "100%", height: "100%" }}/>
+            <Grid container spacing={1} columns={16}>
+                <Grid item xs={8} sm={8} md={8} lg={8} xl={8}>
+                    <Box sx={{
+                        p:1,
+                        display: 'flex',
+                        justifyContent: 'center',
+                        width: "75%",
+                        height:"75%"
+                    }}>
+                        <img src={props.source} alt={props.alt} title={props.title} sx={{ width: "100%", height: "100%" }}/>
+                    </Box>   
+                </Grid>
+                <Grid item xs={8} sm={8} md={8} lg={8} xl={8}>
+                    <Box sx={{
+                        p:2,
+                        m:1,
+                        display: 'flex',
+                        justifyContent: 'center'
+                    }}>
+                        <Typography variant='body1'>{props.title}</Typography>
+                    </Box>
+                </Grid>
+            </Grid>
         </Box>
     )
 }
