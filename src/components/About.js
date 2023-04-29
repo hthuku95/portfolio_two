518d9cf64fd71ba497271cb0366bf669c46a17b3
HARRY OMOLLOH <hthuku95@gmail.com>
2023-04-29 13:57:49 +0300

    Added a new about

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,4 @@
-import {Container,Grid,Typography} from "@mui/material";
+import {Container,Grid,Typography,Box} from "@mui/material";
 import {ReactComponent as AboutLogo} from '../svg/team.svg';
 
 const styles = {
@@ -35,19 +35,13 @@ const About = () =>{
                             color:'custom.violet'
                         }} 
                         gutterBottom='true'>
-                        With a strong background in mathematics and computer science, 
-                        I bring a unique combination of university education and self-taught 
-                        experience to my expertise in web development and blockchain technology. 
-                        I have a thorough understanding of front-end technologies such as HTML/CSS, 
-                        MaterialUI, React.js, Next.js, JavaScript, and TypeScript, as well as 
-                        back-end technologies like Python, Django, and Node.js. My experience in 
-                        blockchain development includes working with Solidity, Hardhat, Brownie, 
-                        Ethers.js, and various EVM compatible chains. I am passionate about the potential 
-                        of blockchain and web3, and have experience in developing and deploying smart 
-                        contracts and decentralized applications. I am constantly staying updated on 
-                        the latest advancements in the field, particularly in the areas of NFTs, 
-                        DeFi, Metaverse, and smart contracts, with a special interest in hybrid-smart 
-                        contracts and SBTs.
+                            With a strong foundation in mathematics and computer science, I bring a unique combination of academic education and self-taught experience to my expertise in web development and blockchain technology. My passion for innovation in the tech industry has led me to delve deeply into the latest advancements in the fields of blockchain, artificial intelligence (AI), and web3.
+
+                            As a software developer, I have an in-depth understanding of front-end technologies such as <Box component="span" sx={{color:'custom.purple'}}>HTML/CSS</Box>, <Box component="span" sx={{color:'custom.purple'}}>MaterialUI</Box>, <Box component="span" sx={{color:'custom.purple'}}>React.js</Box>, <Box component="span" sx={{color:'custom.purple'}}>Next.js</Box>, <Box component="span" sx={{color:'custom.purple'}}>JavaScript</Box>, and <Box component="span" sx={{color:'custom.purple'}}>TypeScript</Box>. I am also well-versed in back-end technologies such as <Box component="span" sx={{color:'custom.purple'}}>Python</Box>, <Box component="span" sx={{color:'custom.purple'}}>Django</Box>, and <Box component="span" sx={{color:'custom.purple'}}>Node.js</Box>. My experience in blockchain development has provided me with knowledge in <Box component="span" sx={{color:'custom.purple'}}>Solidity</Box>, <Box component="span" sx={{color:'custom.purple'}}>Hardhat</Box>, <Box component="span" sx={{color:'custom.purple'}}>Brownie</Box>, <Box component="span" sx={{color:'custom.purple'}}>Ethers.js</Box>, and various EVM compatible chains. I have experience in developing and deploying smart contracts and decentralized applications, and I constantly stay updated on the latest advancements in the field, particularly in the areas of NFTs, DeFi, Metaverse, and smart contracts.
+
+                            My long-term goal is to become a tech entrepreneur and make a meaningful impact in the tech industry. I see myself working in the fields of blockchain or AI, using these technologies to solve some of the world's most pressing problems. I am particularly interested in exploring the potential of hybrid-smart contracts and SBTs, and I believe that these technologies can be instrumental in building a future powered by blockchain and AI.
+
+                            In pursuing my goals, I am committed to staying informed about the latest trends and developments in the field. I actively engage with communities and attend conferences and events to expand my knowledge and build my network. I am excited about the potential of blockchain and AI to revolutionize the way we live and work, and I am eager to be a part of the movement to drive these changes forward.
                     </Typography>
                 </Grid>
             </Grid>
